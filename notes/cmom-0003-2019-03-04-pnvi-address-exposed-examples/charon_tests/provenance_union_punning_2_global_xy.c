ec29ceeace7cb8caa6b8a5e4da04401562603efa
Peter Sewell <Peter.Sewell@cl.cam.ac.uk>
2019-03-11 09:24:30 +0000

    update w.r.t. recent discussion of PNVI-ae* representation-byte and union member reads

--- notes/cmom-0003-2019-03-04-pnvi-address-exposed-examples/charon_tests/provenance_union_punning_2_global_xy.c
+++ notes/cmom-0003-2019-03-04-pnvi-address-exposed-examples/charon_tests/provenance_union_punning_2_global_xy.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <string.h> 
 #include <inttypes.h>
-int x=1, y=2;
-typedef union { uintptr_t ui; int *p; } un;
+int x=1;
+typedef union { uintptr_t ui; int *up; } un;
 int main() {
   un u; 
-  int *px = &x;
-  uintptr_t i = (uintptr_t)px;
-  i = i + sizeof(int);
-  u.ui = i;
-  int *p = u.p;
-  int *q = &y;
-  printf("Addresses: p=%p q=%p\n",(void*)p,(void*)q);
-  if (memcmp(&p, &q, sizeof(p)) == 0) {
-    *p = 11;  // does this have undefined behaviour?
-    printf("x=%d y=%d *p=%d *q=%d\n",x,y,*p,*q);
+  int *p = &x;
+  u.up = p;
+  uintptr_t i = u.ui;
+  int *q = (int*)i;
+  *q = 11;  // does this have undefined behaviour?
+  printf("x=%d *p=%d *q=%d\n",x,*p,*q);
   }
   return 0;
 }
