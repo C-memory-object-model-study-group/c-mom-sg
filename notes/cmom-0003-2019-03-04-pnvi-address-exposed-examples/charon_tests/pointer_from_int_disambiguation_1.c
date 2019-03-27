3b04a138f3f71736dba8a924d0e0a591e2559f26
Peter Sewell <Peter.Sewell@cl.cam.ac.uk>
2019-03-27 15:52:06 +0000

    fix pfid tests

--- notes/cmom-0003-2019-03-04-pnvi-address-exposed-examples/charon_tests/pointer_from_int_disambiguation_1.c
+++ notes/cmom-0003-2019-03-04-pnvi-address-exposed-examples/charon_tests/pointer_from_int_disambiguation_1.c
@@ -6,8 +6,9 @@ int y=2, x=1;
 int main() {
   int *p = &x+1;
   int *q = &y;
+  uintptr_t i = (uintptr_t)p;
+  uintptr_t j = (uintptr_t)q;
   if (memcmp(&p, &q, sizeof(p)) == 0) {
-    uintptr_t i = (uintptr_t)p;
     int *r = (int *)i;
     *r=11;  // is this free of UB?
     printf("x=%d y=%d *p=%d *q=%d *r=%d\n",x,y,*p,*q,*r); 
